aa965dc08a71bd275f7c3dfb5ddd8efaeb0f7c80
adamblumoff <ablumoff25@amherst.edu>
2023-10-10 17:17:26 -0400

    comments added

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import pandas as pd
 text = b'R'
-with open('est.txt', 'rb') as file_in:
+with open('est.txt', 'rb') as file_in: #this section reformats text file into another text file
     with open('output.txt', 'wb') as file_out:
         file_out.writelines(
             filter(lambda line: text in line, file_in)
         )
 
 df = pd.read_csv('output.txt', sep = ' ', header = None, names = ['n/a', 'feed #', 'id', 'date', 'time'])
-del df['n/a']
+del df['n/a'] #This section creates a table from the output text file using pandas
 print(df)
