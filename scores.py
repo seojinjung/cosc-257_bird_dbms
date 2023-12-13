b651daade6a6adf50ece3aecd1468b6aaf780b0b
jackjohncox <jcox25@amherst.edu>
2023-12-12 19:46:58 -0500

    Update scores.py
    Scores.py now fully functional

--- scores.py
+++ scores.py
@@ -12,7 +12,7 @@ from sqlalchemy import create_engine
 #Define method to remove hyphens
 def remove_hyphens(input_string):
     # Use the re.sub() function to replace hyphens with an empty string
-    result_string = re.sub(r'-', '', str(input_string), count=4)
+    result_string = re.sub(r'-', '', str(input_string), count = 4)
     return result_string
 
 
@@ -22,7 +22,7 @@ warnings.simplefilter(action='ignore', category=pd.errors.SettingWithCopyWarning
   
 # Folder Path: **CHANGE FOR WHATEVER FOLDER YOU ARE USING**
 
-path = r"C:\Users\sambl\Dropbox\PC\Downloads\Sei 2022-2023-20231102T002821Z-001\Sei 2022-2023"
+path = r"C:\Users\JackB\Downloads\Bird feeder data 2022-2023\Bird feeder data 2022-2023"
   
 # Change the directory 
 
@@ -44,83 +44,90 @@ def read_text_file(file_path):
 
 df_sql = pd.DataFrame()
 #Iterate through all files in our folder
-
-for file in os.listdir(): 
-
-    # Check whether file is in text format or not 
-
+for folder in os.listdir():
+    print(folder)
+    newpath = path + "\\" + folder
+    print(newpath)
+    os.chdir(newpath)
     
-    if file.endswith(".txt"): 
-        file_path = f"{path}\{file}"
-  
-        # call read text file function 
+
+    for file in os.listdir(): 
+
+        # Check whether file is in text format or not 
+
         
-        read_text_file(file_path)
-        
-
-
-    #Turns text file into dataframe for score calculations
-
-    df = pd.read_csv(output, sep = ' ', header = None, names = ['n/a', 'feed #', 'rfid', 'date', 'time'])
-    df['datetime'] = df['date'] + " " + df['time']
-    df["displace"] = ""
-    df["departure"] = ""
-    df["score"] = 0
-    del df['n/a']
-    #del df['date']
-    del df['time']
-    del df['feed #'] 
-
-
-    #Checks all rows in dataframe
-
-    for ind in df.index:
-
-    
-        #Removes hyphens
-        df['rfid'][ind] = remove_hyphens(df['rfid'][ind])
-        
-        #Stops loop when final calculation can be done
-        if(ind<len(df.index)-2):
-        
-            #Gets timestamp for bird1 and rfid
-        
-            bird_time = datetime.strptime(df["datetime"][ind], '%m/%d/%y %H:%M:%S').timestamp()
-            bird_rfid = df['rfid'][ind]
-    
-            #Gets timestamp for bird2 and rfid
-        
-            bird2_time = datetime.strptime(df["datetime"][ind+1], '%m/%d/%y %H:%M:%S').timestamp()
-            bird2_rfid = df['rfid'][ind+1]
+        if file.endswith(".txt"): 
+            file_path = f"{newpath}\{file}"  
+            # call read text file function 
             
-    
-            #Gets timestamp for bird2's potential departure time and rfid
+            read_text_file(file_path)
+
+        Feeder_name = re.sub(r'[^a-zA-Z]', '', os.path.basename(os.path.dirname(file_path)))
+        print(Feeder_name)
+            
+
+
+        #Turns text file into dataframe for score calculations
+
+        df = pd.read_csv(output, sep = ' ', header = None, names = ['n/a', 'feed #', 'rfid', 'date', 'time'])
+        df['datetime'] = df['date'] + " " + df['time']
+        df["displace"] = ""
+        df["departure"] = ""
+        df["score"] = 0
+        df['feeder'] = Feeder_name
+        del df['n/a']
+        #del df['date']
+        del df['time']
+        del df['feed #'] 
+
+
+        #Checks all rows in dataframe
+
+        for ind in df.index:
+
+            #Stops loop when final calculation can be done
+            df['rfid'][ind] = remove_hyphens(df['rfid'][ind])
+            if(ind<len(df.index)-2):
+            
+                #Gets timestamp for bird1 and rfid
+            
+                bird_time = datetime.strptime(df["datetime"][ind], '%m/%d/%y %H:%M:%S').timestamp()
+                bird_rfid = df['rfid'][ind]
         
-            bird2_dep_time = datetime.strptime(str(df["datetime"][ind+2]), '%m/%d/%y %H:%M:%S').timestamp()
-            bird2_dep_rfid = df['rfid'][ind+2]
+                #Gets timestamp for bird2 and rfid
             
+                bird2_time = datetime.strptime(df["datetime"][ind+1], '%m/%d/%y %H:%M:%S').timestamp()
+                bird2_rfid = df['rfid'][ind+1]
+                
         
-            #Caclulated displacement time for bird1 and bird2
+                #Gets timestamp for bird2's potential departure time and rfid
+            
+                bird2_dep_time = datetime.strptime(str(df["datetime"][ind+2]), '%m/%d/%y %H:%M:%S').timestamp()
+                bird2_dep_rfid = df['rfid'][ind+2]
+
+            
+                #Caclulated displacement time for bird1 and bird2
+            
+                disp_time = bird2_time-bird_time
+                df["displace"][ind+1] = disp_time
         
-            disp_time = bird2_time-bird_time
-            df["displace"][ind+1] = disp_time
-    
-            #If the displacement time is less than or equal to three seconds, and they are different birds, we might have a dominance interaction
-        
-            if(disp_time <=3 and bird_rfid != bird2_rfid):
+                #If the displacement time is less than or equal to three seconds, and they are different birds, we might have a dominance interaction
             
-                #Caculates departure time of bird2 and puts it into the dataframe
-            
-                dep_time = bird2_dep_time - bird2_time
-                df["departure"][ind+2] = dep_time
-            
-                #If bird2's departure is the same bird, and its time is less than or equal to five seconds, then we can assign a point to bird 2 for dominating bird1
-            
-                if(bird2_rfid==bird2_dep_rfid and dep_time<=5):
-                    df["score"][ind+1] = 1
-    df_final = df[['rfid', 'score', 'date']]
-    df_sql = pd.concat([df_final, df_sql])
-    #df_final = df_final.groupby('rfid').sum().reset_index()
+                if(disp_time <=3 and bird_rfid != bird2_rfid):
+                
+                    #Caculates departure time of bird2 and puts it into the dataframe
+                
+                    dep_time = bird2_dep_time - bird2_time
+                    df["departure"][ind+2] = dep_time
+                
+                    #If bird2's departure is the same bird, and its time is less than or equal to five seconds, then we can assign a point to bird 2 for dominating bird1
+                
+                    if(bird2_rfid==bird2_dep_rfid and dep_time<=5):
+                        df["score"][ind+1] = 1
+
+        df_final = df[['rfid', 'score', 'date', 'feeder']]
+        df_sql = pd.concat([df_final, df_sql], ignore_index=True)
+#df_final = df_final.groupby('rfid').sum().reset_index()
 
     
 """
