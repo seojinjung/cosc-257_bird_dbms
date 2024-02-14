f76d17e68782b6213ff727c9fb27bede817f26be
espinosawendy <104166519+espinosawendy@users.noreply.github.com>
2024-02-14 14:52:56 -0500

    Testing - No forever daemon

--- client/src/apis/BirdFinder.js
+++ client/src/apis/BirdFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3006/api/v1/birds"
+    baseURL: "http://cosc-257-node06.cs.amherst.edu:3006/api/v1/birds"
 });
